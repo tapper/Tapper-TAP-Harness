254a0cd633aec8bdc984c7982bdb602401f7a003
Steffen Schwigon <steffen.schwigon@amd.com>
2010-06-02 15:40:51 +0200

    version++

--- lib/Artemis/TAP/Harness.pm
+++ lib/Artemis/TAP/Harness.pm
@@ -8,7 +8,7 @@ use TAP::Parser;
 use TAP::Parser::Aggregator;
 use Directory::Scratch;
 
-our $VERSION = '2.010037';
+our $VERSION = '2.010038';
 our @SUITE_HEADER_KEYS_GENERAL = qw(suite-version
                                     hardwaredb-systems-id
                                     machine-name
