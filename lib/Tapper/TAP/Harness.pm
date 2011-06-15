802439bb2d2b3cf0ea12f7e2052d572b8500873c
Maik Hentsche <maik.hentsche@amd.com>
2011-06-15 16:04:10 +0200

    version++

--- lib/Tapper/TAP/Harness.pm
+++ lib/Tapper/TAP/Harness.pm
@@ -13,7 +13,7 @@ use Archive::Tar;
 use IO::Scalar;
 use IO::String;
 
-our $VERSION = '3.000010';
+our $VERSION = '3.000011';
 
 our @SUITE_HEADER_KEYS_GENERAL = qw(suite-version
                                     hardwaredb-systems-id
